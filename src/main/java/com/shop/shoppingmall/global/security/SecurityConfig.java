ee18066d383dfbe3a18d3e842bc16b2dd58a443d
gudwns0513 <gudwns051613@gmail.com>
2025-04-30 18:32:03 +0900

    feat: 로그아웃 SecurityConfig 작성

--- src/main/java/com/shop/shoppingmall/global/security/SecurityConfig.java
+++ src/main/java/com/shop/shoppingmall/global/security/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.shop.shoppingmall.global.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shop.shoppingmall.domain.user.oauth.OAuth2UserService;
+import com.shop.shoppingmall.global.response.CommonResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +17,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableMethodSecurity
@@ -32,6 +38,26 @@ public class SecurityConfig {
                         .requestMatchers("/api/**").authenticated() // 인증이 필요한 API
                         .anyRequest().permitAll()
                 )
+                .logout(logout -> logout
+                        .logoutUrl("/api/logout") // 로그아웃 요청 URL
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            response.setStatus(HttpServletResponse.SC_OK);
+                            response.setContentType("application/json;charset=UTF-8");
+
+                            // CommonResponse 사용하여 응답 생성
+                            CommonResponse<String> responseBody = CommonResponse.<String>builder()
+                                    .code(HttpStatus.OK.value())
+                                    .data(null)
+                                    .message("로그아웃이 정상적으로 처리되었습니다.")
+                                    .build();
+
+                            // 응답을 JSON으로 작성
+                            response.getWriter().write(new ObjectMapper().writeValueAsString(responseBody));
+                        })
+                        .invalidateHttpSession(true) // 세션 무효화
+                        .deleteCookies("JSESSIONID") // 쿠키 제거
+                )
+
 
                 .oauth2Login(oauth2 -> oauth2
                         .successHandler(successHandler()) // 로그인 성공 처리
